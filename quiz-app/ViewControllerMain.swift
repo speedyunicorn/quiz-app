bba90532abeea8b066f5b9890f05be755532c10f
Speedy <speedyunicorn777@gmail.com>
2016-03-09 21:11:49 -0800

    few more files

--- quiz-app/ViewControllerMain.swift
+++ quiz-app/ViewControllerMain.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ViewControllerMain.swift
 //  quiz-app
 //
 //  Created by Nikita Fill on 3/2/16.
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewControllerMain: UIViewController {
 
+//    @IBOutlet weak var ButtonEasyMode: UIButton!
+//    @IBOutlet weak var ButtonNormalMode: UIButton!
+//    @IBOutlet weak var ButtonHardMode: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        print ("segue.identifier = \(segue.identifier)")
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- quiz-app/ViewControllerQuestions.swift
+++ quiz-app/ViewControllerQuestions.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2016 Nikita Fill. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ViewControllerQuestions: UIViewController {
+    
+    @IBOutlet weak var labelMode: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // if ()
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Check segue identifier and set label to correct value
+        print ("segue.identifier = \(segue.identifier)")
+        if segue.identifier == "modeEasy" {
+            labelMode.text = "Easy Mode"
+        }
+    }
+    
+    override func unwindForSegue(unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
+        print("unwindSegue.identifier = \(unwindSegue.identifier)")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
